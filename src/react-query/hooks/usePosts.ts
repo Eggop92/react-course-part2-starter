beabc0678dfb745c1e3e027238b0e74f60ec241d
e.puerta <e.puerta@speexx.com>
2025-04-29 11:46:36 +0200

    Infinite queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,24 +9,27 @@ interface Post {
 }
 
 interface postQuery {
-    page: number;
     pageSize: number;
 }
 
 const usePosts = (query: postQuery) => {
-    const fetchPosts = () => axios
+    const fetchPosts = ({ pageParam = 1 }) => axios
         .get('https://jsonplaceholder.typicode.com/posts', {
             params: {
-                _start: (query.page - 1) * query.pageSize,
+                _start: (pageParam - 1) * query.pageSize,
                 _limit: query.pageSize
             }
         })
         .then(res => res.data);
 
-    return useQuery<Post[], Error>({
+    return useInfiniteQuery<Post[], Error>({
         queryKey: ['posts', query],
         queryFn: fetchPosts,
-        staleTime: 1 * 60 * 1000
+        staleTime: 1 * 60 * 1000,
+        keepPreviousData: true,
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length > 0 ? allPages.length + 1 : undefined;
+        },
     });
 
 };
