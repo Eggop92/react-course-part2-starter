815b5b03b21f9b8c7caa2a0642b053df49ecdfd8
e.puerta <e.puerta@speexx.com>
2025-04-29 12:44:22 +0200

    Show mutation progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -20,6 +20,7 @@ const TodoForm = () => {
 
       //updating the data in the cache
       queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])]);
+      if (ref.current) ref.current.value = '';
     }
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -43,7 +44,7 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>{addTodo.isLoading ? 'Adding...' : 'Add'}</button>
         </div>
       </form>
     </>
