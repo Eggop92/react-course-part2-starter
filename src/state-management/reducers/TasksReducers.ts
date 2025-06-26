055b4a905cdcaa1ad9d5fe90ce9e83374d80a3e0
e.puerta <e.puerta@speexx.com>
2025-06-26 16:08:08 +0200

    tasks reducers

---
+++ src/state-management/reducers/TasksReducers.ts
@@ -0,0 +1,28 @@
+
+
+interface Task {
+    id: number;
+    title: string;
+}
+
+interface AddTask {
+    type: 'ADD';
+    task: Task;
+}
+interface DeleteTask {
+    type: 'DELETE';
+    taskId: number;
+}
+
+type Action = AddTask | DeleteTask;
+
+const taskReducer = (tasks: Task[], action: Action): Task[] => {
+    switch (action.type) {
+        case 'ADD':
+            return [action.task, ...tasks];
+        case 'DELETE':
+            return tasks.filter(task => task.id !== action.taskId);
+    }
+}
+
+export default taskReducer;
